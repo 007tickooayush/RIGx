da90f0b08fabc78eb5971580a0ecc6f6f14cde1a
Ayush Tickoo <ayush.tickoo282000@gmail.com>
2021-04-04 20:04:33 +0530

    uerlist component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'RIGx';
+    title = 'RIGx';
+    
 }
+//     name = 'ayush';
+
+//     getName() {
+//         return 'peter';
+//     }
+
+//     getObjName() {
+//         return this.name;
+//     }
+
+//     obj = {
+//         name:'kelly',
+//         age:20
+//     };
+
+//     arr = ['bruce','tony','peter'];
+
+//     siteUrl = window.location.href;
+// }
